603ecd36c7e534bb5dcc57cd48a212681f1fa224
DieCode24 <diego.giraldo.0424@gmail.com>
2024-05-25 12:48:42 -0500

    Change details (Name of variable bibliioteca) and add comments

--- sc_sistema-de-informacion-biblioteca/main.py
+++ sc_sistema-de-informacion-biblioteca/main.py
@@ -1,41 +1,43 @@
-# Importamos la clase SistemaBiblioteca del módulo helpers en el paquete utils.
+# Importamos la clase SistemaBiblioteca del módulo helpers en el paquete utils, y lo asocio a la palabra reservada propia sb.
 from utils.helpers import SistemaBiblioteca as sb
 
 def main():
     """
     Función principal que ejecuta el menú principal en un bucle continuo hasta que el usuario decida salir.
-    """
+    """    
     while True:
-        biblioteca_1 = sb()
-        sb.limpiar_consola(biblioteca_1)
-        sb.mostrar_menu(biblioteca_1)
+        # Se insantica la bibliote a utilizar.
+        biblioteca = sb()
+        
+        sb.limpiar_consola(biblioteca)
+        sb.mostrar_menu(biblioteca)
         
         opcion = input("\n\n> Ingrese una opción => ")
         
         if opcion == '1':
-            sb.gestionar_tesis(biblioteca_1)
-            sb.pausar_sistema(biblioteca_1)
+            sb.gestionar_tesis(biblioteca)
+            sb.pausar_sistema(biblioteca)
         elif opcion == '2':
-            sb.gestionar_articulos(biblioteca_1)
-            sb.pausar_sistema(biblioteca_1)
+            sb.gestionar_articulos(biblioteca)
+            sb.pausar_sistema(biblioteca)
         elif opcion == '3':
-            sb.gestionar_libros(biblioteca_1)
-            sb.pausar_sistema(biblioteca_1)
+            sb.gestionar_libros(biblioteca)
+            sb.pausar_sistema(biblioteca)
         elif opcion == '4':
-            sb.gestionar_categorias(biblioteca_1)
-            sb.pausar_sistema(biblioteca_1)
+            sb.gestionar_categorias(biblioteca)
+            sb.pausar_sistema(biblioteca)
         elif opcion == '5':
-            sb.gestionar_autores(biblioteca_1)
-            sb.pausar_sistema(biblioteca_1)
+            sb.gestionar_autores(biblioteca)
+            sb.pausar_sistema(biblioteca)
         elif opcion == '6':
-            sb.gestionar_lectores(biblioteca_1)
-            sb.pausar_sistema(biblioteca_1)
+            sb.gestionar_lectores(biblioteca)
+            sb.pausar_sistema(biblioteca)
         elif opcion == '7':
-            sb.gestionar_prestamos(biblioteca_1)
-            sb.pausar_sistema(biblioteca_1)
+            sb.gestionar_prestamos(biblioteca)
+            sb.pausar_sistema(biblioteca)
         elif opcion == '8':
-            sb.gestionar_multas(biblioteca_1)
-            sb.pausar_sistema(biblioteca_1)
+            sb.gestionar_multas(biblioteca)
+            sb.pausar_sistema(biblioteca)
         elif opcion == '0':
             print("\n\n> Saliendo del sistema...")
             break
