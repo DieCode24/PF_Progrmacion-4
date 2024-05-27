1a34b91fbbf2c2fd15b71b05f55a55b159bde4f2
DieCode24 <diego.giraldo.0424@gmail.com>
2024-05-26 23:03:25 -0500

    Added new features and logic to main.py file

--- sc_sistema-de-informacion-biblioteca/main.py
+++ sc_sistema-de-informacion-biblioteca/main.py
@@ -4,45 +4,33 @@ from utils.helpers import SistemaBiblioteca as sb
 def main():
     """
     Función principal que ejecuta el menú principal en un bucle continuo hasta que el usuario decida salir.
-    """    
+    """
+    # Se insantica la bibliote a utilizar.
+    biblioteca = sb()
+    
+    sb.limpiar_consola(biblioteca)
+    
+    rol = input("¿Es usted un lector, bibliotecario o administrador? ").lower()
+        
+    if rol == "administrador":
+        clave = input("Ingrese la clave de administrador: ")
+    if clave != "clave_admin":
+        print("Clave incorrecta. Acceso denegado.")
+        return
+        
     while True:
-        # Se insantica la bibliote a utilizar.
-        biblioteca = sb()
         
         sb.limpiar_consola(biblioteca)
-        sb.mostrar_menu(biblioteca)
         
-        opcion = input("\n\n> Ingrese una opción => ")
-        
-        if opcion == '1':
-            sb.gestionar_tesis(biblioteca)
-            sb.pausar_sistema(biblioteca)
-        elif opcion == '2':
-            sb.gestionar_articulos(biblioteca)
-            sb.pausar_sistema(biblioteca)
-        elif opcion == '3':
-            sb.gestionar_libros(biblioteca)
-            sb.pausar_sistema(biblioteca)
-        elif opcion == '4':
-            sb.gestionar_categorias(biblioteca)
-            sb.pausar_sistema(biblioteca)
-        elif opcion == '5':
-            sb.gestionar_autores(biblioteca)
-            sb.pausar_sistema(biblioteca)
-        elif opcion == '6':
-            sb.gestionar_lectores(biblioteca)
-            sb.pausar_sistema(biblioteca)
-        elif opcion == '7':
-            sb.gestionar_prestamos(biblioteca)
-            sb.pausar_sistema(biblioteca)
-        elif opcion == '8':
-            sb.gestionar_multas(biblioteca)
-            sb.pausar_sistema(biblioteca)
-        elif opcion == '0':
-            print("\n\n> Saliendo del sistema...")
-            break
+        if rol == "lector":
+            sb.mostrar_menu_lector(biblioteca)
+        elif rol == "bibliotecario":
+            sb.mostrar_menu_bibliotecario(biblioteca)
+        elif rol == "administrador":
+            sb.mostrar_menu_administrador(biblioteca)
         else:
-            print("Opción no válida, intente de nuevo.")
+            print("Rol no reconocido. Intente de nuevo.")
+            break
 
 if __name__ == "__main__":
     main()
