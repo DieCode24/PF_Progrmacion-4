63996978f0264eae8429598ac3f1f87dfb556f81
Daniel Estupiñán <danielestupinan0802@gmail.com>
2024-05-28 16:25:37 -0500

    Add Estado class with different states for library items

---
+++ sc_sistema-de-informacion-biblioteca/clases/estado.py
@@ -0,0 +1,6 @@
+class Estado:
+    DISPONIBLE = "Disponible"
+    PRESTADO = "Prestado en biblioteca"
+    EN_REVISION = "En revisión"
+    EN_REPARACION = "En reparación"
+    NO_DISPONIBLE = "No disponible"
