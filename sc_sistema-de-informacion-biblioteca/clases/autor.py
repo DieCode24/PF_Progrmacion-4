5c247c82677ab6c9e11864d8c4a16eb0dbe77b78
Juan David <j.marulanda1@utp.edu.co>
2024-05-30 00:43:48 -0500

    feat: Agregar método agregar_libro a la clase Autor
    Se agregó el método agregar_libro a la clase Autor para permitir la adición de libros a la lista de libros del autor. Esto mejora la funcionalidad del sistema de información de la biblioteca al permitir la gestión de los libros asociados a cada autor de manera más eficiente.

--- sc_sistema-de-informacion-biblioteca/clases/autor.py
+++ sc_sistema-de-informacion-biblioteca/clases/autor.py
@@ -7,11 +7,15 @@ class Autor:
         self.nacionalidad = nacionalidad
         self.fecha_nacimiento = fecha_nacimiento
         self.libros = []
-        self.cantidad_libros = len(self.libros)
+        self.cantidad_libros = 0
         self.habilitado = True
 
     def __str__(self):
         return print(f"Nombre: {self.nombre}\nNacionalidad: {self.nacionalidad}\nFecha de nacimiento: {self.fecha_nacimiento}\nCantidad de libros: {self.cantidad_libros}\n")
-
+    
+    def agregar_libro(self, libro):
+        self.libros.append(libro)
+        self.cantidad_libros += 1
+        
     def __getattribute__(self, name: str) -> Any:
         pass
