d25006cd7776379fd8cad95fefe77eb39a683883
DieCode24 <diego.giraldo.0424@gmail.com>
2024-05-30 03:12:54 -0500

    Use class date, no datetime, datetime is the library

--- sc_sistema-de-informacion-biblioteca/main.py
+++ sc_sistema-de-informacion-biblioteca/main.py
@@ -3,9 +3,7 @@ from managers.libro_manager import LibroManager
 from managers.articulo_cientifico_manager import ArticuloCientificoManager
 from managers.tesis_manager import TesisManager
 from managers.autor_manager import AutorManager
-from datetime import datetime
-
-
+from datetime import date
 
 def main():
     biblioteca = sb()
@@ -13,6 +11,7 @@ def main():
     articulo_manager = ArticuloCientificoManager()
     Autor = AutorManager()
     Tesis = TesisManager()
+
     sb.limpiar_consola(biblioteca)
     sb.print_brand_sistema(biblioteca)
 
@@ -21,11 +20,13 @@ def main():
     
     while rol not in roles_validos:
         rol = input("¿Es usted un lector, bibliotecario o administrador? ").lower()
+
         if rol not in roles_validos:
             print("Rol no reconocido. Intente de nuevo.")
             
     if rol == "administrador":
         clave = input("Ingrese la clave de administrador: ")
+
         if clave != "clave_admin":
             print("Clave incorrecta. Acceso denegado.")
             return
@@ -34,12 +35,15 @@ def main():
         if rol == "lector":
             sb.mostrar_menu_lector(biblioteca)
             return False
+        
         elif rol == "bibliotecario":
             sb.mostrar_menu_bibliotecario(biblioteca, Autor, libro_manager, articulo_manager, Tesis)
             return False
+        
         elif rol == "administrador":
             sb.mostrar_menu_administrador(biblioteca, Autor, libro_manager, articulo_manager, Tesis)
             return False
+
 
 if __name__ == "__main__":
     main()
