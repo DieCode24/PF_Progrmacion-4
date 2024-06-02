0247f0c3618aa043f96306f792aa2e450bab4f3e
Daniel Estupiñán <danielestupinan0802@gmail.com>
2024-06-02 15:03:55 -0500

    refactor: Remove unused imports and commented code

--- sc_sistema-de-informacion-biblioteca/managers/libro_manager.py
+++ sc_sistema-de-informacion-biblioteca/managers/libro_manager.py
@@ -5,6 +5,7 @@ from clases.autor import Autor
 from utils.validators import validar_input
 from managers.autor_manager import AutorManager
 from managers.data_manager import DataManager
+from utils.helpers import pausar_sistema, limpiar_consola
 
 class LibroManager():
     def __init__(self, data_manager: DataManager):
@@ -24,33 +25,76 @@ class LibroManager():
         self.data_manager.books.append(libro)
 
     def listado_libros(self):
+        print("1. Listar todos los libros")
+        print("2. Listar libros activos")
+        print("3. Listar libros inactivos")
+        print ("0. Volver al menú principal")
+        choice = validar_input("Choose an option: ", int)
+        if choice == "1":
+            limpiar_consola()
+            self.listado_completo()
+            pausar_sistema()
+        elif choice == "2":
+            limpiar_consola()
+            self.listado_libros_activos()
+            pausar_sistema()
+        elif choice == "3":
+            limpiar_consola()
+            self.listado_libros_inactivos()
+            pausar_sistema()
+        elif choice == "0":
+            pass
+        else:
+            limpiar_consola()
+            print("Opción inválida. Intente de nuevo.")
+            pausar_sistema()
+    
+    def listado_completo(self):
         for libro in self.data_manager.books:
             print("Título:", libro.titulo + "\n" + "ISBN:", libro.isbn)
             print("Autores:")
             for autor in libro.autores:
                 print(autor.nombre)
+                
+    def listado_libros_activos(self):
+        for libro in self.data_manager.books:
+            if libro.estado == Estado.DISPONIBLE:
+                print("Título:", libro.titulo + "\n" + "ISBN:", libro.isbn)
+                print("Autores:")
+                for autor in libro.autores:
+                    print(autor.nombre)
+                    
+    def listado_libros_inactivos(self):
+        for libro in self.data_manager.books:
+            if libro.estado == Estado.NO_DISPONIBLE:
+                print("Título:", libro.titulo + "\n" + "ISBN:", libro.isbn)
+                print("Autores:")
+                for autor in libro.autores:
+                    print(autor.nombre)
+                    
     
-    def show_menu(self):
-        while True:
-            print("\nArticle Manager")
-            print("1. Registrar libro")
-            print("2. ver libros")
-            print("0. Back")
-            choice = input("Choose an option: ")
-            if choice == "1":
-                autores = AutorManager.seleccionar_autores(self)
-                self.registrar_libro(autores)
-            elif choice == "2":
-                self.listado_libros()
-            elif choice == "0":
-                break
-            else:
-                print("Invalid choice. Please try again.")
+    
+    # def show_menu(self):
+    #     while True:
+    #         print("\nArticle Manager")
+    #         print("1. Registrar libro")
+    #         print("2. ver libros")
+    #         print("0. Back")
+    #         choice = input("Choose an option: ")
+    #         if choice == "1":
+    #             autores = AutorManager.seleccionar_autores(self)
+    #             self.registrar_libro(autores)
+    #         elif choice == "2":
+    #             self.listado_libros()
+    #         elif choice == "0":
+    #             break
+    #         else:
+    #             print("Invalid choice. Please try again.")
         
     def buscar_libro(self, query: str) -> List[Libro]:
         # buscar libro por título o ISBN y mostrar información
         resultados = []
-        for libro in self.libros:
+        for libro in self.data_manager.books:
             if query.lower() in libro.titulo.lower() or query in libro.isbn:
                 resultados.append(libro)
         print("Resultados de la búsqueda:")
@@ -78,6 +122,14 @@ class LibroManager():
         libro = self.get_libro_by_isbn(isbn)
         if libro:
             libro.estado = Estado.NO_DISPONIBLE
+            
+    def load_mock_data(self):
+        autor1 = Autor("Autor Uno", "Nacionalidad Uno", "Fecha Nacimiento Uno")
+        autor2 = Autor("Autor Dos", "Nacionalidad Dos", "Fecha Nacimiento Dos")
+
+        libro1 = Libro("Ciencia", "Libro de Ciencia", "Primera", 2020, "Editorial Uno", [autor1, autor2], Estado.DISPONIBLE, "123-456-789", ["Español"], 5)
+        libro2 = Libro("Ficción", "Libro de Ficción", "Segunda", 2021, "Editorial Dos", [autor1], Estado.NO_DISPONIBLE, "987-654-321", ["Inglés"], 3)
+        self.data_manager.books = [libro1, libro2]
 
     def realizar_prestamo_libro(self, isbn: str):
         libro = self.get_libro_by_isbn(isbn)
