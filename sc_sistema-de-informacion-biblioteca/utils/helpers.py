a61e6316a70573d96c352899f4f596c5051ef318
DieCode24 <diego.giraldo.0424@gmail.com>
2024-06-03 14:18:25 -0500

    Reubicacion de la funcion validar_contraseña a helpers.py

--- sc_sistema-de-informacion-biblioteca/utils/helpers.py
+++ sc_sistema-de-informacion-biblioteca/utils/helpers.py
@@ -1,6 +1,7 @@
 import os
 
 
+
 def pausar_sistema():
     try:
         command_pause = 'pause' if os.name == 'nt' else 'read -n 1 -s -r -p "Presione cualquier tecla para continuar..."'
@@ -8,6 +9,7 @@ def pausar_sistema():
 
     except Exception as e:
         print(f"Error al pausar el sistema: {e}")
+
 
 def limpiar_consola():
     try:
@@ -20,6 +22,30 @@ def limpiar_consola():
     def __init__(self):
         self.rol_actual = None
 
+
 def print_brand_sistema():
-    print("\tSistema de Información Biblioteca\n\n")
-        
+    print("Sistema de Información Biblioteca\n")
+
+    
+def separador_en_consola():
+    print("----------------------------------------\n")
+    
+
+def validar_contraseña(rol):
+    """
+    Valida la contraseña basada en el rol proporcionado.
+
+    :param rol: El rol del usuario (1: Administrador, 2: Bibliotecario, 3: Lector).
+    :return: True si la contraseña es correcta, False en caso contrario.
+    """
+    if rol == "1":
+        contraseña_correcta = os.getenv("ADMIN_PASSWORD")
+    elif rol == "2":
+        contraseña_correcta = os.getenv("LIBRARIAN_PASSWORD")
+    elif rol == "3":
+        contraseña_correcta = os.getenv("READER_PASSWORD")
+    else:
+        return False
+
+    contraseña = input("\n> Ingrese la contraseña => ")
+    return contraseña == contraseña_correcta
