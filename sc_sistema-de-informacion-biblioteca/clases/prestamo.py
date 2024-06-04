7105ed60c28337a92f4f2131952eec64042f1cb6
Juan David <j.marulanda1@utp.edu.co>
2024-06-03 20:58:40 -0500

    feat: Add id_generator to Prestamo class and update date handling

--- sc_sistema-de-informacion-biblioteca/clases/prestamo.py
+++ sc_sistema-de-informacion-biblioteca/clases/prestamo.py
@@ -1,15 +1,25 @@
 from datetime import date
-from typing import List
+from utils.idgenerator import id_generator
+from clases.estado import Estado
+from datetime import timedelta,  date
 
 
 class Prestamo:
-    def __init__(self, id_prestamo: int,  id_lector: int, id_libro: int, fecha_prestamo: date, fecha_devolucion: date):
-        self._id_prestamo = id_prestamo
+    def __init__(self,  id_lector: int, id_libro: int, fecha_prestamo: date, fecha_devolucion: date):
+        self._id_prestamo = id_generator()
         self._id_lector = id_lector
         self._id_libro = id_libro
-        self._fecha_prestamo = fecha_prestamo
-        self._fecha_devolucion = fecha_devolucion
+        self.dias_prestamo = 0 #la diferencia que hay entre la fecha de devolucion y la fecha de entrega
         
+        self._fecha_prestamo = date.today() #fecha de cuando se hace el prestamo
+        self._fecha_entrega = self.calcular_fecha_entrega(self, 3) #fecha de entrega mas 3 dias
+        self._fecha_devolucion = fecha_devolucion  #fecha en cuando se devolvio
+        
+    def calcular_fecha_entrega(self, dias_prestamo) -> date:
+        return self._fecha_prestamo + timedelta(days=dias_prestamo)
+    
+    
+    
     def IdPrestamo(self):
         return self._id_prestamo
     
