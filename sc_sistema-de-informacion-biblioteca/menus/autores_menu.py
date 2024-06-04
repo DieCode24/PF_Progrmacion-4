e88a8c153f107b5a7d319668a24717b9b94ffd64
DieCode24 <diego.giraldo.0424@gmail.com>
2024-06-03 19:44:26 -0500

    Edicion de navegabilidad, solo lo grafico

--- sc_sistema-de-informacion-biblioteca/menus/autores_menu.py
+++ sc_sistema-de-informacion-biblioteca/menus/autores_menu.py
@@ -1,4 +1,4 @@
-from utils.helpers import pausar_sistema, limpiar_consola
+from utils.helpers import pausar_sistema, limpiar_consola, print_brand_sistema, separador_en_consola
 
 def show_autor_menu(self):
     limpiar_consola()
@@ -6,54 +6,55 @@ def show_autor_menu(self):
     tesis_manager = self.container.tesis_manager
     libro_manager = self.container.libro_manager
     articulo_manager = self.container.articulo_manager
+
     while True:
-        print("# Gestión de Autores")
-        print("---------------------------------------------------")
-        print("Seleccione una opción:\n")
-        print("> [1] Registrar Autor")
-        print("> [2] Listas de Autores")
-        print("> [3] Modificar Autor")
-        print("> [4] Inhabiliar Autor")
-        print("> [5] Habilitar Autor")
-        print("> [6] Datos de prueba (Mock data)")
-        print("> [0] Voler al menú principal")
-        opcion = input("Seleccione una opción: ")
+        limpiar_consola()
+        print_brand_sistema()
+        separador_en_consola()
+
+        print("[-- Menu | Gestión de Autores --]")
+
+        print("\n[1] Registrar Autor")
+        print("[2] Listas de Autores")
+        print("[3] Modificar Autor")
+        print("[4] Inhabiliar Autor")
+        print("[5] Habilitar Autor")
+        print("[6] Datos de prueba (Mock data)")
+        print("[0] Volver al menú principal")
+        opcion = input("\n\n> Ingrese una opción => ")
+
         if opcion == "1":
             limpiar_consola()
             autor_manager.registrar_autor()
             pausar_sistema()
-            
+
         elif opcion == "2":
             limpiar_consola()
             autor_manager.listas_d_autores()
             pausar_sistema()
-            limpiar_consola()
-            
-            
+
         elif opcion == "3":
             limpiar_consola()
             autor_manager.modificar_autor()
             pausar_sistema()
-            limpiar_consola()
-            
+
         elif opcion == "4":
             limpiar_consola()
             autor_manager.inhabilitar_autor()
             pausar_sistema()
-            limpiar_consola()
-            
+
         elif opcion == "5":
             limpiar_consola()
             autor_manager.habilitar_autor()
             pausar_sistema()
-            limpiar_consola()
-            
+
         elif opcion == '6':
             autor_manager.mock()
             pausar_sistema()
-            limpiar_consola()
-            
+
         elif opcion == "0":
-            break
+            return False
+
         else:
-            print("Opción inválida. Por favor, intente de nuevo.")
+            print("\n> Opción inválida, intente de nuevo.")
+            pausar_sistema()
